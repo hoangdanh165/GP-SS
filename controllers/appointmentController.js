0f6bda19f6b16793b46badfdd0de53921093f20d
hoangdanh165 <hoangdanh.165@gmail.com>
2025-05-08 15:47:21 +0700

    add book appointment for customer

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,4 +1,5 @@
 import { getRecipientSocketId, io } from "../socket/socket.js";
+import User from "../models/user.js";
 
 async function sendUpdates(req, res) {
   try {
@@ -23,4 +24,34 @@ async function sendUpdates(req, res) {
   }
 }
 
-export { sendUpdates };
+async function newAppointment(req, res) {
+  try {
+    const { formData } = req.body;
+    console.log(formData);
+
+    const targetUsers = await User.findAll({
+      where: {
+        role_id: [1, 2],
+      },
+      attributes: ["id"],
+    });
+
+    targetUsers.forEach((user) => {
+      const recipientSocketId = getRecipientSocketId(user.id);
+
+      if (recipientSocketId) {
+        io.to(recipientSocketId).emit("newAppoinment", formData);
+        console.log(`Sent new appointment to user ${user.id}`);
+      } else {
+        console.log(`User ${user.id} not online`);
+      }
+    });
+
+    res.status(201).json({ message: "Appointment sent!" });
+  } catch (error) {
+    console.error("Error(s) in sendAppointment:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export { sendUpdates, newAppointment };
--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { sendUpdates } from "../controllers/appointmentController.js";
+import {
+  sendUpdates,
+  newAppointment,
+} from "../controllers/appointmentController.js";
 
 const router = express.Router();
 
 router.post("/", sendUpdates);
+router.post("/new", newAppointment);
 
 export default router;
